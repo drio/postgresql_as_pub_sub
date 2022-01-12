27f48bd7a8c0859652baade19dc3da587dc5206b
David Rio Deiros <driodeiros@gmail.com>
2022-01-12 14:12:00 -0500

    done

--- worker.go
+++ worker.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"bytes"
 	"database/sql"
-	"encoding/json"
-	"fmt"
 	"log"
 	"os"
 	"time"
@@ -21,82 +18,89 @@ type Entry struct {
 	StatusChangeTime sql.NullString
 }
 
-/*
-	1. Listen for channel events
-	2. Use the id from channel and select the table to get new job
-	3. do the work and perform an update
-*/
 func main() {
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
-
-	//db, err := sql.Open("postgres", "user=drio password= dbname=pub_sub_test sslmode=disable")
 	dsn := "postgresql://localhost:5432/pub_sub_test?sslmode=disable"
-	db, err := sql.Open("postgres", dsn)
-	if err != nil {
-		logger.Fatal(err)
-	}
-	defer db.Close()
-	logger.Printf("database connection pool established")
+	chName := "jobs_status_channel"
 
-	entries := make([]*Entry, 0)
-	rows, err := db.Query("SELECT * from ps_jobs")
+	db := connect(dsn, logger)
+	defer db.Close()
+
+	reportProblem := func(ev pq.ListenerEventType, err error) {
+		if err != nil {
+			logger.Println(err.Error())
+		}
+	}
+
+	listener := pq.NewListener(dsn, 10*time.Second, time.Minute, reportProblem)
+	err := listener.Listen(chName)
+	if err != nil {
+		panic(err)
+	}
+
+	logger.Println("Start monitoring PostgreSQL...")
+	for {
+		select {
+		case n := <-listener.Notify:
+			id := n.Extra
+			data, status := runSelect(db, logger, id)
+			if status == "new" {
+				logger.Println("New Job!", id, data, status)
+				doWork(db, id, data)
+			}
+			return
+		case <-time.After(90 * time.Second):
+			logger.Println("Received no events for 90 seconds, checking connection")
+			go func() {
+				listener.Ping()
+			}()
+			return
+		}
+	}
+}
+
+func doWork(db *sql.DB, id, data string) {
+	_, err := db.Exec(`
+		UPDATE
+				ps_jobs
+		SET
+				result = $1,
+				status = $2,
+				worker_name = 'gopher',
+				status_change_time = current_timestamp
+		WHERE
+				id = $3
+	`, 1, "success", id)
+
+	if err != nil {
+		log.Fatalf("could not update row: %v", err)
+	}
+}
+
+func runSelect(db *sql.DB, logger *log.Logger, id string) (string, string) {
+	rows, err := db.Query("SELECT * from ps_jobs where id = $1", id)
 	if err != nil {
 		logger.Fatal(err)
 	}
 	defer rows.Close()
 
-	for rows.Next() {
-		e := new(Entry)
-		if err := rows.Scan(&e.Id, &e.Data, &e.Result, &e.Status, &e.WorkerName, &e.StatusChangeTime); err != nil {
-			panic(err)
-		}
-		entries = append(entries, e)
-		fmt.Println(e.Id, e.Data.String)
-	}
 	if err := rows.Err(); err != nil {
 		panic(err)
 	}
 
-	/* Test LISTEN */
-	reportProblem := func(ev pq.ListenerEventType, err error) {
-		if err != nil {
-			fmt.Println(err.Error())
-		}
-	}
-
-	listener := pq.NewListener(dsn, 10*time.Second, time.Minute, reportProblem)
-	chName := "jobs_status_channel"
-	err = listener.Listen(chName)
-	if err != nil {
+	rows.Next()
+	e := new(Entry)
+	if err := rows.Scan(&e.Id, &e.Data, &e.Result, &e.Status, &e.WorkerName, &e.StatusChangeTime); err != nil {
 		panic(err)
 	}
-
-	fmt.Println("Start monitoring PostgreSQL...")
-	waitForNotification(listener)
-	fmt.Println("Done")
+	return e.Data.String, e.Status.String
 }
 
-func waitForNotification(l *pq.Listener) {
-	for {
-		select {
-		case n := <-l.Notify:
-			fmt.Println("Received data from channel [", n.Channel, "] :")
-			// Prepare notification payload for pretty print
-			var prettyJSON bytes.Buffer
-			err := json.Indent(&prettyJSON, []byte(n.Extra), "", "\t")
-			if err != nil {
-				fmt.Println("Error processing JSON: ", err)
-				return
-			}
-			id := string(prettyJSON.Bytes())
-			fmt.Println("Id is : ", id)
-			return
-		case <-time.After(90 * time.Second):
-			fmt.Println("Received no events for 90 seconds, checking connection")
-			go func() {
-				l.Ping()
-			}()
-			return
-		}
+func connect(dsn string, logger *log.Logger) *sql.DB {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		logger.Fatal(err)
 	}
+	logger.Printf("database connection pool established")
+	return db
 }
